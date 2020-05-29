2be9129dfb2f777e32ccecc7bb40ae124cc1f1e8
Ivy Mao <maoivy01@gmail.com>
2020-05-28 22:02:36 -0400

    styling for courses

--- src/components/courses.js
+++ src/components/courses.js
@@ -1,25 +1,56 @@
 import React from "react"
 
+import "../styles/courses.css"
+
 const Courses = () => (
   <div className="courses-container">
-    spring 2020
-    <ul>
-      <li>6.009</li>
-      <li>6.006</li>
-      <li>6.08</li>
-      <li>21M.301</li>
-    </ul>
-    IAP 2020
-    <ul>
-      <li>6.148</li>
-    </ul>
-    fall 2019
-    <ul>
-      <li>6.042</li>
-      <li>18.022</li>
-      <li>8.02</li>
-      <li>24.900</li>
-    </ul>
+    <div className="semester">spring 2020</div>
+    <div className="classes">
+      <div className="class">
+        <div className="class-num coding">6.009</div>
+        fundamentals of programming
+      </div>
+      <div className="class">
+        <div className="class-num coding">6.006</div>
+        introduction to algorithms
+      </div>
+      <div className="class">
+        <div className="class-num coding">6.08</div>
+        introduction to eecs via interconnected embedded systems
+      </div>
+      <div className="class">
+        <div className="class-num">21M.301</div>
+        harmony and counterpoint
+      </div>
+    </div>
+
+    <div className="semester">iap 2020</div>
+    <div className="classes">
+      <div className="class">
+        <div className="class-num coding">6.148</div>
+        mit web programming competition
+      </div>
+    </div>
+
+    <div className="semester">fall 2019</div>
+    <div className="classes">
+      <div className="class">
+        <div className="class-num coding">6.042</div>
+        mathematics for computer science
+      </div>
+      <div className="class">
+        <div className="class-num">18.022</div>
+        multivariable calculus
+      </div>
+      <div className="class">
+        <div className="class-num">8.02</div>
+        electricity and magnetism
+      </div>
+      <div className="class">
+        <div className="class-num">24.900</div>
+        introduction to linguistics
+      </div>
+    </div>
   </div>
 )
 
--- src/components/projectblock.js
+++ src/components/projectblock.js
@@ -6,10 +6,10 @@ import "../styles/projectblock.css"
 
 const ProjectBlock = () => (
   <div className="projblock-container">
+    <Project title="A SONG OF TIN AND FOIL" desc="an online bullet journal" />
+    <Project title="CHIP" desc="an online bullet journal" />
     <Project title="DAY.LY" desc="an online bullet journal" />
-    <Project title="DAY.LY" desc="an online bullet journal" />
-    <Project title="DAY.LY" desc="an online bullet journal" />
-    <Project title="DAY.LY" desc="an online bullet journal" />
+    <Project title="THE READING REDUX" desc="an online bullet journal" />
     <Project title="DAY.LY" desc="an online bullet journal" />
   </div>
 )
